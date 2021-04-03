085fe37c16cdc810ed254dfdc090b7806e08c897
stsykalovskyi <stsykalovskyi@gmail.com>
2021-04-03 22:16:11 +0300

    start make main page html

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,13 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="main")
      */
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
-        ]);
+        return $this->render('html/html/index.html.twig');
     }
 }
